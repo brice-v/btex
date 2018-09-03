83a63f91f0b9158370da734b57545b74c576ba94
brice-v <30622883+brice-v@users.noreply.github.com>
2018-09-03 00:22:34 -0400

    Create btex.c

---
+++ btex.c
@@ -0,0 +1,548 @@
+/*** LICENSES ***/
+
+/*Copyright (c) 2016, Salvatore Sanfilippo <antirez at gmail dot com>
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+/*
+MIT License
+
+Copyright (c) 2018 Brice Vadnais - btex
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+
+
+/*** INCLUDES ***/
+
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdarg.h>
+
+/*** DEFINES ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+#define BTEX_TAB_STOP 8
+
+#define BTEX_VERSION "0.0.1"
+
+enum editorKey{
+	ARROW_LEFT = 1000,
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN,
+	DEL_KEY,
+	HOME_KEY,
+	END_KEY,
+	PAGE_UP,
+	PAGE_DOWN
+};
+
+/*** DATA ***/
+
+typedef struct erow{
+	int size;
+	int rsize;
+	char *chars;
+	char *render;
+} erow;
+
+struct editorConfig {
+	int cx, cy;
+	int rx;
+	int rowoff;
+	int coloff;
+	int screenrows;
+	int screencols;
+	int numrows;
+	char *filename;
+	erow *row;
+	char statusmsg[80];
+	time_t statusmsg_time;
+	struct termios orig_termios;	
+};
+
+struct editorConfig E;
+
+/*** TERMINAL ***/
+
+void die(const char *s){
+	write(STDIN_FILENO, "\x1b[2J",4);
+	write(STDIN_FILENO, "\x1b[H",3);
+
+	perror(s);
+	exit(1);
+}
+
+void disableRawMode(){
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1){
+		die("tcsetattr");
+	}
+}
+
+void enableRawMode(){
+	if(tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
+	atexit(disableRawMode);
+
+	struct termios raw = E.orig_termios;
+	raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_lflag |= ~(CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+int editorReadKey(){
+	int nread;
+	char c;
+	while((nread = read(STDIN_FILENO, &c, 1))!= 1){
+		if(nread == -1 && errno != EAGAIN) die("read");
+	}
+	
+	if(c=='\x1b'){
+		char seq[3];
+
+		if(read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+		if(read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+		if(seq[0] =='['){
+			if(seq[1] >= '0' && seq[1] <= '9'){
+				if(read(STDIN_FILENO, &seq[2],1)!=1) return '\x1b';
+				if(seq[2] == '~'){
+					switch(seq[1]){
+						case '1': return HOME_KEY;
+						case '3': return DEL_KEY;
+						case '4': return END_KEY;
+						case '5': return PAGE_UP;
+						case '6': return PAGE_DOWN;
+						case '7': return HOME_KEY;
+						case '8': return END_KEY;
+					}
+				}
+			} else {
+				switch(seq[1]){
+					case 'A': return ARROW_UP;
+					case 'B': return ARROW_DOWN;
+					case 'C': return ARROW_RIGHT;
+					case 'D': return ARROW_LEFT;
+					case 'H': return HOME_KEY;
+					case 'F': return END_KEY;
+				}
+			}
+		} else if(seq[0] == 'O'){
+			switch(seq[1]){
+				case 'H': return HOME_KEY;
+				case 'F': return END_KEY;
+			}
+		}
+
+		return '\x1b';
+	} else {
+		return c;
+	}
+
+
+}
+
+int getCursorPosistion(int *rows, int *cols){
+	char buf[32];
+	unsigned int i = 0;
+
+	if(write(STDOUT_FILENO,"\x1b[6n",4)!=4) return -1;
+
+	printf("\r\n");
+	/*char c;*/
+	while(i < sizeof(buf) -1){
+		if(read(STDIN_FILENO,&buf[i],1)!=1) break;
+		if(buf[i] == 'R') break;
+		i++;
+	}
+	buf[i] = '\0';
+
+	if(buf[0] != '\x1b' || buf[1] != '[') return -1;
+	if(sscanf(&buf[2], "%d;%d",rows,cols)!= 2) return -1;
+
+	return 0;
+}
+
+int getWindowSize(int *rows, int *cols){
+	struct winsize ws;
+
+	if(ioctl(STDOUT_FILENO,TIOCGWINSZ,&ws)==-1 || ws.ws_col == 0){
+		if(write(STDOUT_FILENO,"\x1b[999C\x1b[999B",12)!=12) return -1;
+		getCursorPosistion(rows,cols);
+		return -1;
+	} else {
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
+/*** row operations ***/
+
+int editorRowCxToRx(erow *row, int cx){
+	int rx = 0;
+	int j;
+	for(j=0;j<cx;j++){
+		if(row->chars[j]=='\t')
+			rx+=(BTEX_TAB_STOP - 1) - (rx % BTEX_TAB_STOP);
+		rx++;
+	}
+	return rx;
+}
+
+void editorUpdateRow(erow *row){
+	int tabs = 0;
+	int j;
+	for (j = 0; j < row->size; j++)
+		if (row->chars[j] == '\t') tabs++;
+	
+	free(row->render);
+	row->render = malloc(row->size + tabs*(BTEX_TAB_STOP - 1) + 1);
+	int idx = 0;
+	for (j = 0; j < row->size; j++) {
+		if (row->chars[j] == '\t') {
+			row->render[idx++] = ' ';
+		while (idx % BTEX_TAB_STOP != 0) row->render[idx++] = ' ';
+		} else {
+			row->render[idx++] = row->chars[j];
+		}
+	}
+	row->render[idx] = '\0';
+	row->rsize = idx;
+}
+
+void editorAppendRow(char *s, size_t len) {
+	E.row = realloc(E.row,sizeof(erow) * (E.numrows + 1));
+
+	int at = E.numrows;
+	E.row[at].size = len;
+	E.row[at].chars = malloc(len + 1);
+	memcpy(E.row[at].chars, s, len);
+	E.row[at].chars[len] = '\0';
+
+	E.row[at].rsize = 0;
+	E.row[at].render = NULL;
+	editorUpdateRow(&E.row[at]);
+
+	E.numrows++;
+}
+
+/*** FILE IO ***/
+
+void editorOpen(char *filename){
+	free(E.filename);
+	E.filename = strdup(filename);
+
+	FILE *fp = fopen(filename, "r");
+	if(!fp) die("fopen");
+
+	char *line = NULL;
+	size_t linecap = 0;
+	ssize_t linelen;
+	linelen = getline(&line, &linecap, fp);
+	if(linelen != -1) {
+		while((linelen = getline(&line, &linecap,fp))!=-1){
+			while(linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+				linelen--;
+			editorAppendRow(line,linelen);
+		}
+	}
+	free(line);
+	fclose(fp);
+}
+
+/*** APPEND BUFFER ***/
+
+struct abuf
+{
+	char *b;
+	int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len){
+	char *new = realloc(ab->b, ab->len + len);
+
+	if(new == NULL) return;
+	memcpy(&new[ab->len],s,len);
+	ab->b = new;
+	ab->len += len;
+}
+
+void abFree(struct abuf *ab){
+	free(ab->b);
+}
+
+/*** OUTPUT ***/
+
+void editorScroll(){
+	E.rx = 0;
+	if(E.cy < E.numrows){
+		E.rx = editorRowCxToRx(&E.row[E.cy],E.cx);
+	}
+
+	if(E.cy < E.rowoff){
+		E.rowoff = E.cy;
+	}
+	if(E.cy >= E.rowoff + E.screenrows){
+		E.rowoff = E.cy - E.screenrows + 1;
+	}
+	if(E.cx < E.coloff){
+		E.coloff = E.rx;
+	}
+	if(E.cx >= E.coloff + E.screencols){
+		E.coloff = E.rx - E.screencols + 1;
+	}
+}
+
+void editorDrawRows(struct abuf *ab){
+	int y;
+	for(y =0;y<E.screenrows;y++){
+		int filerow = y + E.rowoff;
+		if(filerow>= E.numrows){
+			if(E.numrows == 0 && y== E.screenrows / 3){
+				char welcome[80];
+				int welcomelen = snprintf(welcome,sizeof(welcome),"btex Editor -- version %s",BTEX_VERSION);
+				if(welcomelen > E.screencols) welcomelen = E.screencols;
+				int padding = (E.screencols - welcomelen) / 2;
+				if(padding){
+					abAppend(ab, "~",1);
+					padding--;
+				}
+				while(padding--) abAppend(ab," ", 1);
+				abAppend(ab,welcome,welcomelen);
+			} else {
+				abAppend(ab, "~",1);
+			}
+		} else {
+			int len = E.row[filerow].rsize - E.coloff;
+			if(len < 0) len = 0;
+			if(len > E.screencols) len = E.screencols;
+			abAppend(ab, &E.row[filerow].render[E.coloff], len);
+		}
+
+		abAppend(ab,"\x1b[K",3);
+		abAppend(ab,"\r\n",2);
+	}
+}
+
+void editorDrawStatusBar(struct abuf *ab){
+	abAppend(ab,"\x1b[7m",4);
+	char status[80], rstatus[80];
+	int len = snprintf(status,sizeof(status),"%.20s - %d lines", E.filename ? E.filename:"[NO NAME]",E.numrows);
+	int rlen = snprintf(rstatus,sizeof(status),"%d/%d",E.cy + 1, E.numrows);
+	if(len>E.screencols) len = E.screencols;
+	abAppend(ab,status,len);
+	while(len<E.screencols){
+		if(E.screencols - len == rlen){
+			abAppend(ab, rstatus, rlen);
+			break;
+		} else {
+			abAppend(ab, " " , 1);
+			len++;
+		}
+	}
+	abAppend(ab,"\x1b[m",3);
+	abAppend(ab, "\r\n",2);
+}
+
+void editorRefreshScreen(){
+	editorScroll();
+
+	struct abuf ab = ABUF_INIT;
+
+	abAppend(&ab, "\x1b[?25l",6);
+	abAppend(&ab, "\x1b[H",3);
+
+	editorDrawRows(&ab);
+	editorDrawStatusBar(&ab);
+
+	char buf[32];
+	snprintf(buf,sizeof(buf),"\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
+	abAppend(&ab, buf, strlen(buf));
+
+	abAppend(&ab,"\x1b[?25h",6);
+
+	write(STDIN_FILENO, ab.b, ab.len);
+	abFree(&ab);
+}
+
+void editorSetStatusMessage(const char *fmt, ...){
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(E.statusmsg, sizeof(E.statusmsg),fmt,ap);
+	va_end(ap);
+	E.statusmsg_time = time(NULL);
+}
+
+/*** INPUT ***/
+
+void editorMoveCursor(int key){
+	erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+	switch(key){
+		case ARROW_LEFT:
+			if(E.cx != 0){
+				E.cx--;	
+			} else if (E.cy > 0) {
+			    E.cy--;
+			    E.cx = E.row[E.cy].size;
+			}
+			break;
+		case ARROW_RIGHT:
+			if(row && E.cx < row->size){
+				E.cx++;
+			} else if (row && E.cx == row->size) {
+		        E.cy++;
+		        E.cx = 0;
+		    }
+			break;
+		case ARROW_UP:
+			if(E.cy != 0){
+				E.cy--;
+			}
+			break;
+		case ARROW_DOWN:
+			if(E.cy < E.numrows){
+				E.cy++;	
+			}
+			break;
+
+	row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];	
+	int rowlen = row ? row->size : 0;
+	if(E.cx > rowlen){
+		E.cx = rowlen;
+	}
+	}
+}
+
+void editorProcessKeyPress(){
+	int c = editorReadKey();
+
+	switch(c){
+		case CTRL_KEY('q'):
+			write(STDIN_FILENO, "\x1b[2J",4);
+			write(STDIN_FILENO, "\x1b[H",3);
+			exit(0);
+			break;
+
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+		case END_KEY:
+			if(E.cy < E.numrows)
+				E.cx = E.row[E.cy].size;
+			break;
+
+		case PAGE_UP:
+		case PAGE_DOWN:
+			{
+				if(c==PAGE_UP){
+					E.cy = E.rowoff;
+				} else if(c== PAGE_DOWN){
+					E.cy = E.rowoff + E.screenrows - 1;
+					if(E.cy > E.numrows) E.cy = E.numrows;
+				}
+				int times = E.screenrows;
+				while(times--)
+					editorMoveCursor(c == PAGE_UP ? ARROW_UP: ARROW_DOWN);
+			}
+			break;
+
+
+		case ARROW_UP:
+		case ARROW_DOWN:
+		case ARROW_LEFT:
+		case ARROW_RIGHT:
+			editorMoveCursor(c);
+			break;
+	}
+}
+
+/*** INIT ***/
+
+void initEditor(){
+	E.cx = 0;
+	E.cy = 0;
+	E.rx = 0;
+	E.rowoff = 0;
+	E.coloff = 0;
+	E.numrows = 0;
+	E.row = NULL;
+	E.filename = NULL;
+	E.statusmsg[0] = '\0';
+	E.statusmsg_time = 0;
+
+	if(getWindowSize(&E.screenrows,&E.screencols) == -1) die("getWindowSize");
+	E.screenrows -= 2;
+}
+
+int main(int argc, char *argv[]){
+	enableRawMode();
+	initEditor();
+	if(argc >= 2){
+		editorOpen(argv[1]);
+	}
+
+	editorSetStatusMessage("HELP - CTRL-Q to Quit");
+
+	while(1){
+		editorRefreshScreen();
+		editorProcessKeyPress();
+	}
+	
+	return 0;
+}
